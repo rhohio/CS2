13bb055376400f826e3df42f220a4602bd48a2df
Rhohio <kad145@zips.uakron.edu>
2019-04-23 12:04:44 -0400

    project init

---
+++ PROJECT/src/main.cpp
@@ -0,0 +1,12 @@
+/*
+   COPYRIGHT (C) 2018 Student Name (kad145) All rights reserved.
+   CS Project
+   Author.  Kyle Dabbieri
+            kad145@zips.uakron.edu
+   Version. 1.01 04.23.2019
+   Purpose: Restaurant management
+*/
+
+
+
+
