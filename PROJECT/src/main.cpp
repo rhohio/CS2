d182b8d178cc55355d3852e8804f8f3bff683774
Rhohio <kad145@zips.uakron.edu>
2019-04-24 15:55:12 -0400

    light work on project

--- PROJECT/src/main.cpp
+++ PROJECT/src/main.cpp
@@ -4,9 +4,26 @@
    Author.  Kyle Dabbieri
             kad145@zips.uakron.edu
    Version. 1.01 04.23.2019
-   Purpose: Restaurant management
+   Purpose: Restaurant management Project
 */
 
+#include <string>
 
+
+
+    /*
+     *      <globalConstants>
+     */
+//Number of Restaurants
+const int NUMHFCS = 8;
+//Number of tables in each restaurant
+const int HFCSIZES[NUMHFCS] = {19, 15, 24, 33, 61, 17, 55, 37};
+//Names of restaurants
+const std::string RESTAURANT[NUMHFCS] = {"The Salubrious Bistro", "What the Kale?",
+                                         "Lettuce Enjoy", "Vegan Nights",
+                                         "Pasta is a Planet", "The Meatless Miracle",
+                                         "The Pesky Pescatarian", "The Fine Fettle"};
+//Number of meals
+const int NUMMEALS = 10;
 
 
