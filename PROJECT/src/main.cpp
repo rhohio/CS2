111074e7de427e73b1e3c0850ffa7c0db3fda648
Rhohio <kad145@zips.uakron.edu>
2019-05-06 16:35:13 -0400

    option 4 is all that is left

--- PROJECT/src/main.cpp
+++ PROJECT/src/main.cpp
@@ -1,4 +1,4 @@
-wwwwww/*
+/*
    COPYRIGHT (C) 2018 Student Name (kad145) All rights reserved.
    CS Project
    Author.  Kyle Dabbieri
@@ -70,6 +70,15 @@ void freeTable(Restaurant **[], std::queue<Restaurant>&);
 int assignTableNumber(Restaurant**[], short int);
 int generatePatronID(Restaurant **[]);
 void writeToLog(Restaurant &, std::fstream &, std::map<int, Meals>&);
+void findPatron(Restaurant **[], std::fstream&);
+short int findMealNumberFromObj(Restaurant **[], std::string&);
+std::string findFavoriteMeal(std::fstream&, std::string&, short int);
+void displayUsage(Restaurant **[], std::fstream&);
+void findFavoriteRestaurants(Restaurant **[], std::fstream&);
+void findFavoriteMeals(Restaurant **[], std::fstream&);
+void findFavoriteTimes(Restaurant **[], std::fstream&);
+short int getMode(std::vector<short int> &);
+void displayLog(std::fstream&);
 
 int main()
 {
@@ -80,6 +89,7 @@ int main()
     if(!log.is_open())
     {
         std::cerr << "Log file failed to open/generate." << std::endl;
+        return -1;
     }
 
     //makes an array, that points to NUMHFCS arrays, which are arrays of size HFCSIZE[i], that point to rest objs
@@ -89,22 +99,22 @@ int main()
         restPtr[i] = new Restaurant *[HFCSIZES[i]];
     }
 
-    //TODO: Fix this shit
-
+    //create map of meals using parallel arrays
     std::map<int, Meals> mealMap;
     for(int i = 0; i < NUMMEALS; ++i)
     {
         mealMap.insert(std::make_pair<int, Meals>(int(i), Meals(Meals(MEALNAMES[i], CALORIES[i], PROTEIN[i], VITAMINS[i]))));
     }
 
+    //create waiting list queue
     std::queue<Restaurant> waitingList;
 
     bool isMainRepeated = true;
 
     do
     {
-        int menuAnswer = mainMenu();
-        assessMenuAnswer(menuAnswer, isMainRepeated, restPtr, mealMap, waitingList, log);
+        //calls main menu display, then assess the answer based on input
+        assessMenuAnswer(mainMenu(), isMainRepeated, restPtr, mealMap, waitingList, log);
     }while(isMainRepeated);
 
 }
@@ -112,6 +122,7 @@ int main()
 
 int mainMenu()
 {
+    //display main menu
     std::cout << " " << std::setfill('_') << std::setw(50) << " " << std::endl
               << "|" << std::setfill('-') << std::setw(50) << "|" << std::endl
               << "|" << std::setfill(' ') << std::setw(17) << " " << "Kyle Dabbieri's" << std::setw(18) << "|" << std::endl
@@ -126,6 +137,7 @@ int mainMenu()
               << "|" << std::setw(17) << " " << "6) Quit" << std::setw(26) << "|" << std::endl
               << "|" << std::setfill('_') << std::setw(50) << "|" << std::endl
               << "Please enter a choice from the menu [6 to quit]: ";
+    //validate main menu
     int menuAnswer;
     bool invalidMenuAnswer = false;
     do
@@ -137,11 +149,13 @@ int mainMenu()
             invalidMenuAnswer = true;
         }
     }while(invalidMenuAnswer);
+
     return menuAnswer;
 }
 
 void assessMenuAnswer(int menuAnswer, bool &mainLoopFlag, Restaurant **restPtr[], std::map<int, Meals> &mealMap, std::queue<Restaurant> &waitingList, std::fstream &log)
 {
+    //based on menu answer, go to appropriate function
     switch(menuAnswer)
     {
         case 1:
@@ -151,13 +165,13 @@ void assessMenuAnswer(int menuAnswer, bool &mainLoopFlag, Restaurant **restPtr[]
             freeTable(restPtr, waitingList);
             break;
         case 3:
-            //findPatron();
+            findPatron(restPtr, log);
             break;
         case 4:
-            //displayUsage();
+            displayUsage(restPtr, log);
             break;
         case 5:
-            //displayLog();
+            displayLog(log);
             break;
         case 6:
             mainLoopFlag = false;
@@ -172,6 +186,7 @@ void requestTable(Restaurant **restPtr[], std::map<int, Meals> &mealMap, std::qu
 {
     std::cout << "Please enter the restaurant choice from 1 through 8 : ";
 
+    //get and validate res choice
     int resAnswer;
     bool invalidRestAnswer = false;
     do
@@ -183,8 +198,10 @@ void requestTable(Restaurant **restPtr[], std::map<int, Meals> &mealMap, std::qu
             invalidRestAnswer = true;
         }
     }while(invalidRestAnswer);
+
     std::cout << "You have chosen " << RESTAURANT[resAnswer - 1] << std::endl;
 
+    //assigns table number if possible, if full, assigns -1
     int tableNumber = assignTableNumber(restPtr, resAnswer);
 
     //restaurant is full
@@ -193,7 +210,7 @@ void requestTable(Restaurant **restPtr[], std::map<int, Meals> &mealMap, std::qu
         std::cout << "This restaurant is currently full. You will be placed into the wait list and given a table as soon as possible." << std::endl
                   << "Please enter your name: ";
 
-        //create restaurant obj to hold data from patron until table is avail
+        //create restaurant obj to hold data from patron to place into queue
         Restaurant patron;
 
         patron.setResNumber(resAnswer);
@@ -222,6 +239,7 @@ void requestTable(Restaurant **restPtr[], std::map<int, Meals> &mealMap, std::qu
         }
 
         std::cout << "Meal: ";
+
         bool isMealNameInvalid = false;
         short int mealName;
         do
@@ -233,21 +251,27 @@ void requestTable(Restaurant **restPtr[], std::map<int, Meals> &mealMap, std::qu
                 isMealNameInvalid = true;
             }
         }while(isMealNameInvalid);
+
         patron.setPatronMealNumber(mealName);
+
         std::cout << "Your choice was the " << mealMap[mealName].getPatronMealName() << std::endl
                   << mealMap[mealName].getCalories() << " calories, "
                   << mealMap[mealName].getProtein()  << " grams of protein, and "
                   << mealMap[mealName].getVitamins() << " milligrams of vitamins" << std::endl;
         std::cout << "Thank you for your patience, you've been added to the wait list." << std::endl;
+
+        //push data from patron on the waiting list
         waitingList.push(patron);
     }
+    //restaurant isn't full
     else
     {
-
+        //create new restaurant obj and set values we already know
         restPtr[resAnswer][tableNumber] = new Restaurant;
         restPtr[resAnswer][tableNumber]->setResNumber(resAnswer);
         restPtr[resAnswer][tableNumber]->setTableNumber(tableNumber);
         restPtr[resAnswer][tableNumber]->setPatronID(generatePatronID(restPtr));
+
         std::cout << "Your table number is " << tableNumber + 1 << " and your ID number is " //TODO SETFILL FOR ID
                   <<  restPtr[resAnswer][tableNumber]->getPatronID() << std::endl
                   << "Please enter the name of the Patron: ";
@@ -265,6 +289,7 @@ void requestTable(Restaurant **restPtr[], std::map<int, Meals> &mealMap, std::qu
         }while(isPatronNameInvalid);
 
         restPtr[resAnswer][tableNumber]->setPatronName(patronName);
+
         std::cout << "Please enter the meal number from the following choices: " << std::endl;
         for(int i = 0; i < NUMMEALS; ++i)
         {
@@ -297,8 +322,8 @@ void requestTable(Restaurant **restPtr[], std::map<int, Meals> &mealMap, std::qu
         std::cin.ignore();
         std::cin.get();
 
+        //write info from patron into log
         writeToLog(*restPtr[resAnswer][tableNumber], log, mealMap);
-
     }
 }
 
@@ -342,6 +367,7 @@ void freeTable(Restaurant **restPtr[], std::queue<Restaurant> &waitingList)
 
     std::cout << restPtr[resNum][tableNum]->getPatronName() << " has enjoyed another meal. Goodbye!" << std::endl;
 
+    //free the memory allocated to the obj it was pointing at
     delete restPtr[resNum][tableNum];
 
     //if queue is empty, nothing to do
@@ -355,16 +381,19 @@ void freeTable(Restaurant **restPtr[], std::queue<Restaurant> &waitingList)
         restPtr[resNum][tableNum] = new Restaurant(waitingList.front());
         waitingList.pop();
     }
-    //if front isnt the right restaurant
+    //if front isnt the right restaurant, and the queue isn't empty
     else
     {
+        //stack to hold data from the queue we want to keep
         std::stack<Restaurant> tempRestStack;
+
         //keep popping off the queue and saving the objs until you find the right restaurant
         while(waitingList.front().getResNumber() != resNum)
         {
             tempRestStack.push(Restaurant(waitingList.front()));
             waitingList.pop();
         }
+
         //once we've found the right rest, replace old patron, and pop it off
         restPtr[resNum][tableNum] = new Restaurant(waitingList.front());
         waitingList.pop();
@@ -380,13 +409,16 @@ void freeTable(Restaurant **restPtr[], std::queue<Restaurant> &waitingList)
 
 int assignTableNumber(Restaurant **restPtr[], short int restaurant)
 {
+    //iterate through the restuarants tables
     for(int i = 0; i < HFCSIZES[restaurant]; ++i)
     {
+        //if there isn't an obj, then there's a free table
         if(!(restPtr[restaurant]))
         {
             return i;
         }
     }
+    //if no free tables, return -1
     return -1;
 }
 
@@ -403,9 +435,9 @@ int generatePatronID(Restaurant **restPtr[])
         newID = rand() % 100000;
         for(int i = 0; i < NUMHFCS; ++i)
         {
-            for (int j = 0; j < HFCSIZES[restPtr[i]->getResNumber()]; ++j)
+            for (int j = 0; j < HFCSIZES[restPtr[i][0]->getResNumber()]; ++j)
             {
-                if (newID == restPtr[j]->getPatronID())
+                if (newID == restPtr[i][j]->getPatronID())
                 {
                     isUnique = false;
                 }
@@ -417,10 +449,224 @@ int generatePatronID(Restaurant **restPtr[])
 
 void writeToLog(Restaurant &input, std::fstream &log, std::map<int, Meals> &mealMap)
 {
-    //TODO WRITE TO LOG FILE [Patron Name, Meal Time, Patron Meal Name, Calories, Protein, Vitamins] in separate function
+    log << input.getResNumber() << '\t' << input.getPatronName() << '\t' << input.getMealTime() << '\t' << MEALNAMES[input.getPatronMealNumber()] << '\t'
+        << mealMap[input.getPatronMealNumber()].getCalories() << '\t' << mealMap[input.getPatronMealNumber()].getProtein()
+        << '\t' << mealMap[input.getPatronMealNumber()].getVitamins() << std::endl;
+}
 
-    log << input.getPatronName() << '\t' << input.getMealTime() << '\t' << MEALNAMES[input.getPatronMealNumber()] << '\t'
+void findPatron(Restaurant **restPtr[], std::fstream &log)
+{
+    std::cout << "Please enter the patron by name: ";
+    bool isPatronNameInvalid = false;
+    std::string patronName;
+    do
+    {
+        std::cin >> patronName;
+        if(patronName.empty())
+        {
+            std::cout << "Please enter a valid name (Not empty): ";
+            isPatronNameInvalid = true;
+        }
+    }while(isPatronNameInvalid);
+
+    std::string favoriteMeal = findFavoriteMeal(log, patronName, findMealNumberFromObj(restPtr, patronName));
+
+    if(favoriteMeal.empty())
+    {
+        std::cout << patronName << " has no favorite." << std::endl;
+    }
+    else
+    {
+        std::cout << patronName << " likes " << favoriteMeal << std::endl;
+    }
+
+    std::cout << "Hit enter for next selection" << std::endl;
+    std::cin.ignore();
+    std::cin.get();
+}
+
+short int findMealNumberFromObj(Restaurant **restPtr[], std::string &patronName)
+{
+    for(int i = 0; i < NUMHFCS; ++i)
+    {
+        for(int j = 0; j < HFCSIZES[i]; ++j)
+        {
+            if(restPtr[i][j]->getPatronName() == patronName)
+            {
+                return restPtr[i][j]->getPatronMealNumber();
+            }
+        }
+    }
+    return -1;
+}
+
+//TODO: Finish findFavoriteMeal
+std::string findFavoriteMeal(std::fstream &log, std::string &patronName, short int mealNumber)
+{
+    std::string _patronName;
+    std::vector<std::string> mealNames;
+    std::string mealName;
+    while(!log.eof())
+    {
+        log.ignore('\t');
+        log >> _patronName;
+        if(_patronName == patronName)
+        {
+            log.ignore('\t');
+            getline(log, mealName, '\t');
+            mealNames.push_back(mealName);
+            log.ignore('\n');
+        }
+    }
+
+    for(auto meal: mealNames)
+    {
+        if(meal == MEALNAMES[0])
+        {
+
+        }
+    }
+}
+
+void displayUsage(Restaurant **restPtr[], std::fstream &log)
+{
+    std::cout << "A) Favorite restaurants \nB) Favorite meals \nC)Favorite days and times \n" << std::endl
+              << "Pick your favorite report from the list: ";
+
+    char menuAns;
+    bool isMenuAnsInvalid = false;
+    do
+    {
+        std::cin >> menuAns;
+        if(menuAns != 'a' || menuAns != 'A' || menuAns != 'b' || menuAns != 'B' || menuAns != 'c' || menuAns != 'C')
+        {
+            std::cout << "Please enter either an A, B, or C: ";
+            isMenuAnsInvalid = true;
+        }
+    }while(isMenuAnsInvalid);
+
+    switch(menuAns)
+    {
+        case 'a':
+        case 'A':
+            findFavoriteRestaurants(restPtr, log);
+            break;
+        case 'b':
+        case 'B':
+            findFavoriteMeals(restPtr, log);
+            break;
+        case 'c':
+        case 'C':
+            findFavoriteTimes(restPtr, log);
+            break;
+        default:
+            return;
+    }
+}
+
+
+/*
+ * log << input.getPatronName() << '\t' << input.getMealTime() << '\t' << MEALNAMES[input.getPatronMealNumber()] << '\t'
         << mealMap[input.getPatronMealNumber()].getCalories() << '\t' << mealMap[input.getPatronMealNumber()].getProtein()
         << '\t' << mealMap[input.getPatronMealNumber()].getVitamins() << std::endl;
 
+ */
+
+//TODO: THIS
+void findFavoriteRestaurants(Restaurant **restPtr[], std::fstream &log)
+{
+    std::map<short int, int> favoriteRestaurants;
+    int count = 0;
+    for(int i = 0; i < NUMHFCS; ++i)
+    {
+        for(int j = 0; j < HFCSIZES[i]; ++j)
+        {
+            favoriteRestaurants.insert(std::make_pair(restPtr[i][j]->getResNumber(), count));
+        }
+    }
+
+    short int resNum;
+
+    while(!log.eof())
+    {
+        log.ignore('\t');
+        log.ignore('\t');
+        log >> resNum;
+        favoriteRestaurants.push_back(resNum);
+        log.ignore('\n');
+    }
+
+    getMode(favoriteRestaurants);
+
+}
+
+void findFavoriteMeals(Restaurant **restPtr[], std::fstream &log)
+{
+
+}
+
+
+void findFavoriteTimes(Restaurant **restPtr[], std::fstream &log)
+{
+
+}
+
+
+short int getMode(std::vector<short int> &input)
+{
+    short int count[input.size()];
+
+}
+
+/*
+    log << input.getResNumber() << '\t' << input.getPatronName() << '\t' << input.getMealTime() << MEALNAMES[input.getPatronMealNumber()] << '\t'
+        << mealMap[input.getPatronMealNumber()].getCalories() << '\t' << mealMap[input.getPatronMealNumber()].getProtein()
+        << '\t' << mealMap[input.getPatronMealNumber()].getVitamins() << std::endl;
+*/
+
+
+void displayLog(std::fstream &log)
+{
+    std::cout << "Which restaurant would you like to view the log for? [1 - " << NUMHFCS << "]: ";
+
+    short int _resNum;
+    bool isResAnsInvalid = false;
+    do
+    {
+        std::cin >> _resNum;
+        if(_resNum < 1 || _resNum > NUMHFCS)
+        {
+            std::cout << "Please enter a valid restaurant number [1 - " << NUMHFCS << "]: ";
+            isResAnsInvalid = true;
+        }
+    }while(isResAnsInvalid);
+
+    _resNum--;
+
+    short int resNum;
+    std::string patronName;
+    std::string mealTime;
+    std::string mealName;
+    short int calories;
+    short int protein;
+    short int vitamins;
+
+    while(!log.eof())
+    {
+        log >> resNum;
+        if(resNum == _resNum)
+        {
+            log >> patronName;
+            log >> mealTime;
+            log >> mealName;
+            log >> calories;
+            log >> protein;
+            log >> vitamins;
+
+            std::cout << "Log entries for " << RESTAURANT[resNum] << std::endl
+                      << patronName << " had " << mealName << " with "
+                      << calories << " calories, " << protein << " grams of protein, and "
+                      << vitamins << " mg of vitamins at " << mealTime << std::endl;
+        }
+    }
 }
